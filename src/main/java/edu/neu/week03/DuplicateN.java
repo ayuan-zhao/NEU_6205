57e72a82ac85381575915f00b5e5b34f6fa04bd7
Ayuan Zhao <ayuan@Ayuans-Mac.local>
2022-02-10 22:49:57 -0800

    week03

--- src/main/java/edu/neu/week03/DuplicateN.java
+++ src/main/java/edu/neu/week03/DuplicateN.java
@@ -9,62 +9,66 @@ package edu.neu.week03;
  * @year 2021
  */
 
-    /************************************************************
-     Nothing can be changed in class DuplicateN
-     *************************************************************/
-    public class DuplicateN {
-        private int[] a ; //a[i] guaranteed > 0 && < n-1
-        //contents of a should be exactly like the original after your algorithms
-        private String algName ;
-        private int [] numDup ;
-        private double [] work ;
-        private boolean show ; //show only is show = true
+/************************************************************
+ Nothing can be changed in class DuplicateN
+ *************************************************************/
+public class DuplicateN {
 
-        DuplicateN(int[] a, String algName,int [] numDup,double [] work, boolean show) {
-            this.a = a ;
-            this.algName = algName;
-            this.numDup = numDup;
-            this.work = work ;
-            this.show = show ;
-            AlgduplicateN b = new AlgduplicateN(this,algName,show) ;
-        }
+    private int[] a; //a[i] guaranteed > 0 && < n-1
+    //contents of a should be exactly like the original after your algorithms
+    private String algName;
+    private int[] numDup;
+    private double[] work;
+    private boolean show; //show only is show = true
 
-        public int size() {
-            return a.length ;
-        }
-        public int getI(int i) {
-            work[0]++ ;
-            return a[i];
-        }
-        public void setI(int i, int v) {
-            work[0]++ ;
-            a[i] = v ;
-        }
-
-        public void setnumDup(int v) {
-            numDup[0] = v ;
-        }
+    DuplicateN(int[] a, String algName, int[] numDup, double[] work, boolean show) {
+        this.a = a;
+        this.algName = algName;
+        this.numDup = numDup;
+        this.work = work;
+        this.show = show;
+        AlgduplicateN b = new AlgduplicateN(this, algName, show);
     }
+
+    public int size() {
+        return a.length;
+    }
+
+    public int getI(int i) {
+        work[0]++;
+        return a[i];
+    }
+
+    public void setI(int i, int v) {
+        work[0]++;
+        a[i] = v;
+    }
+
+    public void setnumDup(int v) {
+        numDup[0] = v;
+    }
+}
 
 /************************************************************
  WRITE CLASS AlgduplicateN
  YOU CAN HAVE ANY NUMBER OF PRIVATE VARIABLES and FUNCTIONS
  *************************************************************/
 class AlgduplicateN {
+
     private DuplicateN f;
     private String algName;
-    private boolean show ; //show only is show = true
+    private boolean show; //show only is show = true
 
     AlgduplicateN(DuplicateN f, String algName, boolean show) {
         //NOTHING CAN BE CHANGED HERE
-        this.f = f ;
+        this.f = f;
         this.show = show;
         if (algName == "NsquareTimeConstantSpace") {
             alg_nsquare_time_constant_space();
-        }else if (algName == "NTimeNSpace") {
+        } else if (algName == "NTimeNSpace") {
             //alg_nsquare_time_constant_space();
             alg_ntime_n_space();
-        }else if (algName == "NTimeConstantSpace") {
+        } else if (algName == "NTimeConstantSpace") {
             //alg_nsquare_time_constant_space();
             alg_ntime_constant_space();
         }
--- src/main/java/edu/neu/week03/DuplicateNTest.java
+++ src/main/java/edu/neu/week03/DuplicateNTest.java
@@ -6,168 +6,175 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 /**
-     * File Name: DuplicateNTest.java
-     * Sum of N base class
-     *
-     * To Compile: IntUtil.java RandomInt.java DuplicateN.java DuplicateNTest.java
-     *
-     * @author Jagadeesh Vasudevamurthy
-     * @year 2021
-     */
+ * File Name: DuplicateNTest.java Sum of N base class
+ *
+ * To Compile: IntUtil.java RandomInt.java DuplicateN.java DuplicateNTest.java
+ *
+ * @author Jagadeesh Vasudevamurthy
+ * @year 2021
+ */
 
-    /*
-     * YOU CANNOT CHANGE ANYTHING IN THIS FILE. READ ONLY
-     */
+/*
+ * YOU CANNOT CHANGE ANYTHING IN THIS FILE. READ ONLY
+ */
 
-    public class DuplicateNTest {
-        //input
-        private int[] a ; //a[i] guaranteed > 0 && < n-1
-        //contents of a should be exactly like the original after your algorithms
-        private boolean show ; //show only is show = true
-        private IntUtil u = new IntUtil();
+public class DuplicateNTest {
 
-        DuplicateNTest() {
-            testBed() ;
+    //input
+    private int[] a; //a[i] guaranteed > 0 && < n-1
+    //contents of a should be exactly like the original after your algorithms
+    private boolean show; //show only is show = true
+    private IntUtil u = new IntUtil();
+
+    DuplicateNTest() {
+        testBed();
+    }
+
+    private void testBed() {
+        simpleTests();
+        randomTests();
+        System.out.println("All test cases passed. You will get full grade now");
+        System.out.println("Include only DuplicateN.java and the output of this program for FULL grade");
+    }
+
+    private void test1() {
+        if (show) {
+            System.out.println("----------------------------");
+            u.pLn(a.length);
+            u.pLn(a);
         }
-
-        private void testBed() {
-            simpleTests() ;
-            randomTests() ;
-            System.out.println("All test cases passed. You will get full grade now");
-            System.out.println("Include only DuplicateN.java and the output of this program for FULL grade");
+        int[] copya = u.copyArray(a);
+        int[] d0 = {-1}; //Number of duplicates from alg 0
+        int[] d1 = {-1}; //Number of duplicates from alg 1
+        int[] d2 = {-1}; //Number of duplicates from alg 2
+        {
+            long startTime = System.nanoTime();
+            double[] work0 = {0}; //Work for alg 0
+            DuplicateN da = new DuplicateN(a, "NsquareTimeConstantSpace", d0, work0, show);
+            long endTime = System.nanoTime();
+            double d = u.timeInSec(endTime, startTime);
+            System.out.println(
+                "O(n^2)Time O(1)Space Alg.  n = " + a.length + " Work = " + work0[0] + " CPU = " + d
+                    + " secs");
+            boolean e = u.arrayEqual(a, copya);
+            if (e == false) {
+                System.out.println("You changed the content of given array a");
+                u.myassert(false);
+            }
+            u.myassert(e);
         }
-
-        private void test1() {
-            if (show) {
-                System.out.println("----------------------------");
-                u.pLn(a.length) ;
-                u.pLn(a);
+        {
+            long startTime = System.nanoTime();
+            double[] work1 = {0}; //Work for alg 1
+            DuplicateN da = new DuplicateN(a, "NTimeNSpace", d1, work1, show);
+            long endTime = System.nanoTime();
+            double d = u.timeInSec(endTime, startTime);
+            System.out.println(
+                "O(n)Time O(n)Space Alg.    n = " + a.length + " Work = " + work1[0] + " CPU = " + d
+                    + " secs");
+            boolean e = u.arrayEqual(a, copya);
+            if (e == false) {
+                System.out.println("You changed the content of given array a");
+                u.myassert(false);
             }
-            int[] copya = u.copyArray(a);
-            int [] d0 = {-1} ; //Number of duplicates from alg 0
-            int [] d1 = {-1} ; //Number of duplicates from alg 1
-            int [] d2 = {-1} ; //Number of duplicates from alg 2
-            {
-                long startTime = System.nanoTime();
-                double [] work0 = {0} ; //Work for alg 0
-                DuplicateN da = new DuplicateN(a,"NsquareTimeConstantSpace",d0,work0,show) ;
-                long endTime = System.nanoTime();
-                double d = u.timeInSec(endTime,startTime) ;
-                System.out.println("O(n^2)Time O(1)Space Alg.  n = " + a.length + " Work = " + work0[0]  + " CPU = " + d + " secs") ;
-                boolean  e = u.arrayEqual(a, copya);
-                if (e == false) {
-                    System.out.println("You changed the content of given array a") ;
-                    u.myassert(false) ;
-                }
-                u.myassert(e);
-            }
-            {
-                long startTime = System.nanoTime();
-                double [] work1 = {0} ; //Work for alg 1
-                DuplicateN da = new DuplicateN(a,"NTimeNSpace",d1,work1,show) ;
-                long endTime = System.nanoTime();
-                double d = u.timeInSec(endTime,startTime) ;
-                System.out.println("O(n)Time O(n)Space Alg.    n = " + a.length + " Work = " + work1[0] + " CPU = " + d + " secs") ;
-                boolean  e = u.arrayEqual(a, copya);
-                if (e == false) {
-                    System.out.println("You changed the content of given array a") ;
-                    u.myassert(false) ;
-                }
-                u.myassert(e);
-                u.myassert(d1[0] == d0[0]);
-            }
-            {
-                long startTime = System.nanoTime();
-                double [] work2 = {0} ; //Work for alg 2
-                DuplicateN da = new DuplicateN(a,"NTimeConstantSpace",d2,work2,show) ;
-                long endTime = System.nanoTime();
-                double d = u.timeInSec(endTime,startTime) ;
-                System.out.println("O(n)Time O(1)Space Alg.    n = " + a.length +  " Work = " + work2[0] + " CPU = " + d + " secs") ;
-                boolean  e = u.arrayEqual(a, copya);
-                if (e == false) {
-                    System.out.println("You changed the content of given array a") ;
-                    u.myassert(false) ;
-                }
-                u.myassert(e);
-                u.myassert(d2[0] == d0[0]);
-            }
+            u.myassert(e);
+            u.myassert(d1[0] == d0[0]);
         }
-
-        private void simpleTests() {
-            show = true ;
-            {
-                int b[] = { 1,1,1 };
-                this.a = b ;
-                test1();
+        {
+            long startTime = System.nanoTime();
+            double[] work2 = {0}; //Work for alg 2
+            DuplicateN da = new DuplicateN(a, "NTimeConstantSpace", d2, work2, show);
+            long endTime = System.nanoTime();
+            double d = u.timeInSec(endTime, startTime);
+            System.out.println(
+                "O(n)Time O(1)Space Alg.    n = " + a.length + " Work = " + work2[0] + " CPU = " + d
+                    + " secs");
+            boolean e = u.arrayEqual(a, copya);
+            if (e == false) {
+                System.out.println("You changed the content of given array a");
+                u.myassert(false);
             }
-            {
-                int b[] = { 1,0,0,0 };
-                this.a = b ;
-                test1();
-            }
-            {
-                int b[] = { 1,2,3,1,3,6,6 };
-                this.a = b ;
-                test1();
-            }
-            {
-                int b[] = { 1,2,3,1,3,0,0 };
-                this.a = b ;
-                test1();
-            }
-            {
-                int b[] = { 0,0,1 };
-                this.a = b ;
-                test1();
-            }
-            {
-                int b[] = { 3,2,0,1 };
-                this.a = b ;
-                test1();
-            }
-            System.out.println("All simple tests passed");
-        }
-
-        private void randomTests() {
-            show = false ;
-            int N = 500000; //500000 500000
-            for (int i = 10000; i < N; i = i + 10000) {
-                {
-                    a = new int[i];
-                    for (int j = 0; j < i; ++j) {
-                        a[j] = i - 1;
-                    }
-                    test1() ;
-                }
-                {
-                    int start = 0 ;
-                    int end = i-1 ; ;
-                    int [] b = u.generateRandomNumber(i, true, start,end);
-                    this.a = b ;
-                    test1();
-                }
-            }
-            System.out.println(N + " Random tests passed");
-        }
-
-        public static void main(String[] args) {
-            //Change path below
-            String s = "C:\\Users\\jag\\OneDrive\\vasu\\work\\java11\\objects\\afi\\DuplicateN\\duplicateNoutput.txt";
-            if (false) { //Make it to true to write to a file
-                System.out.println("Writing to file" + s) ;
-                try {
-                    System.setOut(new PrintStream(new FileOutputStream(s)));
-                } catch (FileNotFoundException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("DuplicateNTest.java STARTS");
-            String version = System.getProperty("java.version");
-            System.out.println("Java version used for this program is " + version);
-            DuplicateNTest m = new DuplicateNTest();
-            System.out.println("DuplicateNTest.java ENDS");
+            u.myassert(e);
+            u.myassert(d2[0] == d0[0]);
         }
     }
+
+    private void simpleTests() {
+        show = true;
+        {
+            int b[] = {1, 1, 1};
+            this.a = b;
+            test1();
+        }
+        {
+            int b[] = {1, 0, 0, 0};
+            this.a = b;
+            test1();
+        }
+        {
+            int b[] = {1, 2, 3, 1, 3, 6, 6};
+            this.a = b;
+            test1();
+        }
+        {
+            int b[] = {1, 2, 3, 1, 3, 0, 0};
+            this.a = b;
+            test1();
+        }
+        {
+            int b[] = {0, 0, 1};
+            this.a = b;
+            test1();
+        }
+        {
+            int b[] = {3, 2, 0, 1};
+            this.a = b;
+            test1();
+        }
+        System.out.println("All simple tests passed");
+    }
+
+    private void randomTests() {
+        show = false;
+        int N = 500000; //500000 500000
+        for (int i = 10000; i < N; i = i + 10000) {
+            {
+                a = new int[i];
+                for (int j = 0; j < i; ++j) {
+                    a[j] = i - 1;
+                }
+                test1();
+            }
+            {
+                int start = 0;
+                int end = i - 1;
+                ;
+                int[] b = u.generateRandomNumber(i, true, start, end);
+                this.a = b;
+                test1();
+            }
+        }
+        System.out.println(N + " Random tests passed");
+    }
+
+    public static void main(String[] args) {
+        //Change path below
+        String s = "C:\\Users\\jag\\OneDrive\\vasu\\work\\java11\\objects\\afi\\DuplicateN\\duplicateNoutput.txt";
+        if (false) { //Make it to true to write to a file
+            System.out.println("Writing to file" + s);
+            try {
+                System.setOut(new PrintStream(new FileOutputStream(s)));
+            } catch(FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        System.out.println("DuplicateNTest.java STARTS");
+        String version = System.getProperty("java.version");
+        System.out.println("Java version used for this program is " + version);
+        DuplicateNTest m = new DuplicateNTest();
+        System.out.println("DuplicateNTest.java ENDS");
+    }
+}
 
 
